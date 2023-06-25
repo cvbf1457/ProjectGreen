976824f136a2340d7da6a293b10adc2a6a135364
lint4587 <pser3793@gmail.com>
2023-06-25 20:02:07 +0800

    topbar

--- web/js/Information.js
+++ web/js/Information.js
@@ -1,24 +1,23 @@
 function closeForm() {
 	$('#activity_form').removeClass('is-visible');
-  }
-  
-  $(document).ready(function($) {
-	
+}
+
+$(document).ready(function ($) {
+
 	/* Contact Form Interactions */
-	$('#activity_btn').on('click', function(event) {
-	  event.preventDefault();
-  
-	  $('#activity_form').addClass('is-visible');
-	});
-	
-	  //close popup when clicking x or off popup
-	$('#activity_form').on('click', function(event) {
-	  if ($(event.target).is('#activity_form') || $(event.target).is('#btnCloseForm')) {
+	$('#activity_btn').on('click', function (event) {
 		event.preventDefault();
-		$(this).removeClass('is-visible');
-	  }
+
+		$('#activity_form').addClass('is-visible');
 	});
-	
-	
+
+	//close popup when clicking x or off popup
+	$('#activity_form').on('click', function (event) {
+		if ($(event.target).is('#activity_form') || $(event.target).is('#btnCloseForm')) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
 	});
-  
+
+
+});
